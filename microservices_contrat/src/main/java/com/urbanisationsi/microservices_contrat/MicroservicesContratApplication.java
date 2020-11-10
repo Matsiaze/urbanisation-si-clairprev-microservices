e42817c29a5aa640080a29ad7580739a488d7911
ClaudiaMatsiaze <tmatsiaze@gmail.com>
2020-11-10 14:13:08 +0100

    config pom et eureka pour les autres ms

--- microservices_contrat/src/main/java/com/urbanisationsi/microservices_contrat/MicroservicesContratApplication.java
+++ microservices_contrat/src/main/java/com/urbanisationsi/microservices_contrat/MicroservicesContratApplication.java
@@ -2,10 +2,12 @@ package com.urbanisationsi.microservices_contrat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicesContratApplication {
 
 	public static void main(String[] args) {
--- microservices_produit/src/main/java/com/urbanisationsi/microservices_produit/MicroservicesProduitApplication.java
+++ microservices_produit/src/main/java/com/urbanisationsi/microservices_produit/MicroservicesProduitApplication.java
@@ -2,10 +2,12 @@ package com.urbanisationsi.microservices_produit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicesProduitApplication {
 
 	public static void main(String[] args) {
