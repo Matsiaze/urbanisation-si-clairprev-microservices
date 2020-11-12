b029816282f99310c3c65700ec457ae0e9f6c1f5
ClaudiaMatsiaze <tmatsiaze@gmail.com>
2020-11-12 15:12:22 +0100

    last updates with feignCongig auth on msClient

---
+++ microservices_clientui/src/main/java/com/urbanisationsi/microservices_clientui/configuration/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.urbanisationsi.microservices_clientui.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+
+@Configuration
+public class FeignConfig {
+@Bean
+	public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor(){
+		return  new BasicAuthRequestInterceptor("utilisateur", "mdp");
+	}
+}
